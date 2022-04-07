74e59e38c77e2c3348981710f30c00692354764f
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-04-07 12:36:04 +0530

    useEffect

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //useState Hook
   const [counter, setCounter] = useState(0);
+
+  //useEffect Hook
+  useEffect(()=> {
+    console.warn("warning from useEffect", counter)
+    if(counter===5){
+      alert("Counter updated to 5")
+    }
+  },[counter])
+
   return (
     <View style={styles.container}>
       <Text>Counter</Text>
       <Text>{counter}</Text>
-      <Button title='Click Me' onPress={()=> setCounter(counter+1)}/>
+      <Button title='+' onPress={()=> setCounter(counter+1)}/>
+      <Button title='-' onPress={()=> setCounter(counter-1)}/>
     </View>
   );
 }
