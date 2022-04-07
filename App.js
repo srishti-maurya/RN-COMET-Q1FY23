068cbf39c9982ddd1d44fa1074776b88235c999c
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-04-07 16:35:15 +0530

    useContext

--- App.js
+++ App.js
@@ -1,33 +1,51 @@
-import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { ThemeProvider, useTheme } from "./src/context/theme-context";
 
 export default function App() {
+  //context
+  const { theme, setTheme } = useTheme();
+
   //useState Hook
   const [counter, setCounter] = useState(0);
 
   //useEffect Hook
-  useEffect(()=> {
-    console.warn("warning from useEffect", counter)
-    if(counter===5){
-      alert("Counter updated to 5")
+  useEffect(() => {
+    console.warn("warning from useEffect", counter);
+    if (counter === 5) {
+      alert("Counter updated to 5");
     }
-  },[counter])
+  }, [counter]);
 
   return (
-    <View style={styles.container}>
-      <Text>Counter</Text>
-      <Text>{counter}</Text>
-      <Button title='+' onPress={()=> setCounter(counter+1)}/>
-      <Button title='-' onPress={()=> setCounter(counter-1)}/>
-    </View>
+    <ThemeProvider>
+      <View style={styles.container}>
+        <View
+          style={{ backgroundColor: theme === "light" ? "white" : "black" }}
+        >
+          <Text>Counter</Text>
+          <Text>{counter}</Text>
+          <Button title="+" onPress={() => setCounter(counter + 1)} />
+          <Button title="-" onPress={() => setCounter(counter - 1)} />
+          <Button
+            title="Change Theme"
+            onPress={() =>
+              setTheme((theme) => {
+                theme === "dark" ? "light" : "dark";
+              })
+            }
+          />
+        </View>
+      </View>
+     </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
