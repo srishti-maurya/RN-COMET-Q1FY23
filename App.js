292436f7ddf9eea6c96588997364c8a5f60e36f7
srishti-maurya <39724354+srishti-maurya@users.noreply.github.com>
2022-04-07 12:21:47 +0530

    useState

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>React Native Initial Setup</Text>
+      <Text>Counter</Text>
+      <Text>{counter}</Text>
+      <Button title='Click Me' onPress={()=> setCounter(counter+1)}/>
     </View>
   );
 }
